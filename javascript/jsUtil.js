120f1209a2e02ab10194cc95b15aeb33b1cff603
Michael Crain <michaelcrain.apps@gmail.com>
2014-06-19 08:36:55 -0700

    Initialize

---
+++ javascript/jsUtil.js
@@ -0,0 +1,8 @@
+/* QueryString Start */
+    function getQuerystringParameterByName(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+/* QueryString End */
